e50df9dc5f27704de94b5adec46ee65876ae0154
Enrique <enroiv@hotmail.com>
2015-06-24 17:24:35 -0700

    From laptop

--- Conductor/enro/utils/ModCalc.js
+++ Conductor/enro/utils/ModCalc.js
@@ -1,6 +1,10 @@
 /**
  * New node file
  */
-var ModCalc = function(){};
+var ModCalc = function(n){
+	if(n/n !== 1){
+		throw new TypeError("Invalid modulus: " + n);
+	}
+};
 
 module.exports = ModCalc;
